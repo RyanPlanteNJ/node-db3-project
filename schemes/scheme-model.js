0810dbfdf844ea1f07814ac4232fe1cbf3bbdb84
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-28 22:56:00 -0400

    db is not a function.....but it is

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,36 @@
 const db = require('../data/db-config.js');
 
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('scehmes')
+  .where({ id });
+}
+
+function findSteps(id) {
+  return db('steps')
+  .where('scheme_id', id)
+}
+
+function add(schemeData) {
+  return db('schemes')
+  .insert(schemeData)
+}
+
+function update(changes, id) {
+  return db('schemes')
+  .update(changes)
+  .where('id', id)
+}
+
+function remove(id) {
+  return db('schemes')
+  .del()
+  .where('id', id)
+}
+
 module.exports = {
   find,
   findById,
