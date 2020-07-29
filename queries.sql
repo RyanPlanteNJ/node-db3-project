a579187b99d32e7896819601af7d48544ad73b02
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-28 21:46:50 -0400

    answering query questions and help file for schema

--- queries.sql
+++ queries.sql
@@ -2,8 +2,28 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName, [c].CategoryName
+FROM Product as p
+JOIN Category as c
+ON  [c].Id = p.CategoryId;
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+SELECT o.Id,  s.CompanyName
+FROM Shipper as s
+JOIN [Order] as o
+ON o.ShipVia = s.Id
+WHERE o.OrderDate < "2012-08-09"
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+SELECT p.Id, p.ProductName, od.Quantity
+FROM Product as p
+JOIN OrderDetail as od
+ON p.Id = od.ProductId
+WHERE od.OrderId = 10251
+ORDER BY asc
+
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
